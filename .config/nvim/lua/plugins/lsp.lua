357cc4f8791c5bf4d6dc69452e37470030bef053
max-quinn-cko <max.quinn@checkout.com>
2025-10-03 17:44:28 +0100

    updated plugins and add ghostty cursor shader

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,66 +2,70 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = { enabled = false },
       servers = {
         eslint = {
           settings = {
             workingDirectories = { mode = "auto" },
           },
         },
-        -- vtsls = {
-        --   settings = {
-        --     typescript = {
-        --       preferences = {
-        --         importModuleSpecifier = "non-relative",
-        --       },
-        --     },
-        --   },
-        -- },
-        ts_ls = {
-          init_options = {
-            preferences = {
-              importModuleSpecifierPreference = "non-relative",
-              importModuleSpecifierEnding = "minimal",
-            },
-          },
+        vtsls = {
           settings = {
             typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "none",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = false,
-                includeInlayVariableTypeHints = false,
-                includeInlayPropertyDeclarationTypeHints = false,
-                includeInlayFunctionLikeReturnTypeHints = false,
-                includeInlayEnumMemberValueHints = false,
-              },
-            },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "none",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = false,
-                includeInlayVariableTypeHints = false,
-                includeInlayPropertyDeclarationTypeHints = false,
-                includeInlayFunctionLikeReturnTypeHints = false,
-                includeInlayEnumMemberValueHints = false,
+              preferences = {
+                importModuleSpecifier = "non-relative",
               },
             },
           },
         },
+        -- ts_ls = {
+        --   init_options = {
+        --     preferences = {
+        --       importModuleSpecifierPreference = "non-relative",
+        --       importModuleSpecifierEnding = "minimal",
+        --     },
+        --   },
+        --   settings = {
+        --     typescript = {
+        --       inlayHints = {
+        --         includeInlayParameterNameHints = "none",
+        --         includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        --         includeInlayFunctionParameterTypeHints = false,
+        --         includeInlayVariableTypeHints = false,
+        --         includeInlayPropertyDeclarationTypeHints = false,
+        --         includeInlayFunctionLikeReturnTypeHints = false,
+        --         includeInlayEnumMemberValueHints = false,
+        --       },
+        --     },
+        --     javascript = {
+        --       inlayHints = {
+        --         includeInlayParameterNameHints = "none",
+        --         includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        --         includeInlayFunctionParameterTypeHints = false,
+        --         includeInlayVariableTypeHints = false,
+        --         includeInlayPropertyDeclarationTypeHints = false,
+        --         includeInlayFunctionLikeReturnTypeHints = false,
+        --         includeInlayEnumMemberValueHints = false,
+        --       },
+        --     },
+        --   },
+        -- },
       },
       setup = {
         eslint = function()
-          -- automatically fix linting errors on save (but otherwise do not format the document)
-          vim.cmd([[
-          autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll
-        ]])
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
         end,
       },
     },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "typescript-language-server" })
